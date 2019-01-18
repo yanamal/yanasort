6ad971a98fbbfd95e4a05c03be54d035503eb737
Yana <kuziavra@gmail.com>
2019-01-17 18:59:26 -0600

    Initial sort game

---
+++ static/yanasort.js
@@ -0,0 +1,96 @@
+years = [
+  [2003, 12],
+  [2005, 06],
+  [2012, 06],
+  [2006, 01],
+  [2004, 06],
+  [2018, 09],
+  [2017, 03],
+  [2012, 10],
+  [2014, 01],
+  [2015, 06],
+  [2013, 05],
+  [2009, 10],
+  [2010, 08],
+  [2007, 11],
+  [2008, 01],
+  [2016, 11]
+]
+
+// Whether the two pictures represented by the indices are in the right chronological order
+function is_right_order(i1, i2) {
+  y1 = years[i1];
+  y2 = years[i2];
+  return y1[0]<y2[0] || (y1[0]===y2[0] && y1[1]<y2[1])
+}
+
+function reset_hints() {
+  $( "#feedback span" ).text('');
+  $( "#answer span" ).text('')
+}
+
+function show_answer() {
+  $( "#sortable img" ).each(function( index, element ) {
+    let pic_i = element.getAttribute('index');
+    $( "#answer span" ).eq(index).text(years[pic_i][1]+' / '+years[pic_i][0]);
+  });
+}
+
+$( function() {
+  
+  
+  // sortable elements:
+  let w = ($( "#sortable" ).width())/16
+  
+  // $( "#sortable" ).height(270)
+  
+  for(let i=0; i<16; i++) {
+    $( "#sortable" ).append('<img src="yanas/'+i+'.jpg" index='+i+' width="'+w+'"/>')
+  }
+  // $( "#sortable.img" ).css('vertical-align', 'bottom')
+  
+  $( "#sortable" ).sortable({
+    start: function(e, ui){
+      reset_hints()
+      ui.placeholder.height(ui.item.height());
+      // ui.item.width(270);
+      // ui.item.height(270);
+      // console.log(ui.item.attr('src'))
+    }
+  });
+  $( "#sortable" ).disableSelection();
+  
+  
+  // Set up feedback & answer sections:
+  for(let i=0; i<15; i++) {
+    $( "#feedback" ).append('<span align=right style="display: inline-block;color: red;font-weight: bold; width: '+w+'px "></span>')
+  }
+  for(let i=0; i<16; i++) {
+    $( "#answer" ).append('<span style="display: inline-block; width: '+w+'px "></span>')
+  }
+  
+  
+  // Check (and give feedback):
+  $( "#check" ).click(function() {
+    reset_hints()
+    num_wrong = 0
+    $( "#sortable img" ).each(function( index, element ) {
+      let next = element.nextSibling
+      if(next) {
+        let this_i = element.getAttribute('index');
+        let next_i = next.getAttribute('index');
+        if (!is_right_order(this_i, next_i)) {
+          $( "#feedback span" ).eq(index).text('>');
+          num_wrong += 1;
+        }
+      }
+    });
+    if(num_wrong === 0) {
+      show_answer();
+    }
+  });
+  
+  
+  $( "#answer_btn" ).click(show_answer);
+  
+} );
